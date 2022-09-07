ffc4cad1a2c298affc5805ffc1705e57a71726d6
wangxidong <onesitdown@163.com>
2022-09-07 19:26:44 +0800

    add bpexample

---
+++ AgoraBlueprintExample/Source/AgoraBPExample/AgoraBPExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AgoraBPExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AGORABPEXAMPLE_API AAgoraBPExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
