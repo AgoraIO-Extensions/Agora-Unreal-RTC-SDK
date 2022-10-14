4421a5e671e4b35ccef16e3057390b485a6e40e5
wangxidong <onesitdown@163.com>
2022-10-14 18:04:10 +0800

    add files

--- Agora-Unreal-SDK-CPP-Example/Source/AgoraExample.Target.cs
+++ Agora-Unreal-SDK-CPP-Example/Source/AgoraExample.Target.cs
@@ -9,6 +9,11 @@ public class AgoraExampleTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
+		if (Target.Platform == UnrealTargetPlatform.IOS)
+		{
+			bOverrideBuildEnvironment = true;
+			GlobalDefinitions.Add("FORCE_ANSI_ALLOCATOR=1");
+		}
 		ExtraModuleNames.AddRange( new string[] { "AgoraExample" } );
 	}
 }
