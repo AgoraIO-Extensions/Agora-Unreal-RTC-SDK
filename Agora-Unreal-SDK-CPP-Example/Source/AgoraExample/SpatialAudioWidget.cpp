aaa2803af3dca0b2eb922df750e1ef42e7370f1a
wangxidong <onesitdown@163.com>
2022-12-12 19:40:16 +0800

    fix pos

--- Agora-Unreal-SDK-CPP-Example/Source/AgoraExample/SpatialAudioWidget.cpp
+++ Agora-Unreal-SDK-CPP-Example/Source/AgoraExample/SpatialAudioWidget.cpp
@@ -105,7 +105,7 @@ void USpatialAudioWidget::OnJoinButtonClick()
 
 void USpatialAudioWidget::RightMoveButtonClick()
 {	
-	RemoteVoicePositionInfo remoteVoicePos{ { 0.0f, -1.0f, 0.0f }, { 0.0f, 0.0f, 0.0f } };
+	RemoteVoicePositionInfo remoteVoicePos{ { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f, 0.0f } };
 
 	auto ret = LocalSpatialAudioEngine->updateRemotePosition(RemoteUid, remoteVoicePos);
 
@@ -114,7 +114,7 @@ void USpatialAudioWidget::RightMoveButtonClick()
 
 void USpatialAudioWidget::LeftMoveButtonClick()
 {
-	RemoteVoicePositionInfo remoteVoicePos{ { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f, 0.0f } };
+	RemoteVoicePositionInfo remoteVoicePos{ { 0.0f, -1.0f, 0.0f }, { 0.0f, 0.0f, 0.0f } };
 
 	auto ret = LocalSpatialAudioEngine->updateRemotePosition(RemoteUid, remoteVoicePos);
 
