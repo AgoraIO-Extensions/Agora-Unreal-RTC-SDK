c563a4122fe6c01fd55d2e6e9d742429b4093a1a
WinterPu <striveant@outlook.com>
2023-08-10 16:51:22 +0800

    Update Blueprint Example to 4.2.1
    Currently only offer basic demo:
    
    List
    
    * BasicAudioCallScene
    * BasicVideoCallScene
    * ScreenShare
    * ScreenShareWhileVideoCall

--- Agora-Unreal-SDK-Blueprint-Example/Source/AgoraBPExample/AgoraBPExample.Build.cs
+++ Agora-Unreal-SDK-Blueprint-Example/Source/AgoraBPExample/AgoraBPExample.Build.cs
@@ -1,5 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System.IO;
+using System;
 using UnrealBuildTool;
 
 public class AgoraBPExample : ModuleRules
@@ -12,12 +14,22 @@ public class AgoraBPExample : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+
+        if (Target.Platform == UnrealTargetPlatform.Android)
+        {
+            PrivateDependencyModuleNames.AddRange(new string[] { "AndroidPermission" });
+            string UPLFilePath = Path.Combine(ModuleDirectory, "UPL", "AgoraExample_UPL.xml");
+            Console.WriteLine("AgoraExample UPL Path: " + UPLFilePath);
+            // Modify AndroidMenifest.xml
+            AdditionalPropertiesForReceipt.Add("AndroidPlugin", UPLFilePath);
+        }
+    }
 }
