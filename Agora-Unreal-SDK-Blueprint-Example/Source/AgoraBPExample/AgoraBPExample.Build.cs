0d25c180506c473ab41b683c19c006b68f3934e8
WinterPu <striveant@outlook.com>
2023-10-11 17:19:37 +0800

    【BPExample 421】 Use CustomTileView to modify the thickness of scrollbar Enable UICore

--- Agora-Unreal-SDK-Blueprint-Example/Source/AgoraBPExample/AgoraBPExample.Build.cs
+++ Agora-Unreal-SDK-Blueprint-Example/Source/AgoraBPExample/AgoraBPExample.Build.cs
@@ -15,7 +15,7 @@ public class AgoraBPExample : ModuleRules
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
         // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
         // Uncomment if you are using online features
         // PrivateDependencyModuleNames.Add("OnlineSubsystem");
