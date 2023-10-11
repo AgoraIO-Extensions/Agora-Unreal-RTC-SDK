e5373db9036cdffac6b42ff1b10348bd2bdc04be
WinterPu <striveant@outlook.com>
2023-10-11 11:43:27 +0800

    【Example 421】 UI Modification: increase the thickness of the scrollbar in TileView & ScrollBox

--- Agora-Unreal-SDK-CPP-Example/Source/AgoraExample/Examples/MainAgoraUserWidget.cpp
+++ Agora-Unreal-SDK-CPP-Example/Source/AgoraExample/Examples/MainAgoraUserWidget.cpp
@@ -4,6 +4,28 @@
 #include "MainAgoraUserWidget.h"
 #include "GameFramework/GameUserSettings.h"
 
+#pragma region CustomTileView
+
+
+UCustomTileView::UCustomTileView(const FObjectInitializer& Initializer) :Super(Initializer)
+{
+	BarThickness = FVector2D(50, 50);
+}
+
+TSharedRef<STableViewBase> UCustomTileView::RebuildListWidget()
+{
+	auto TileViewRef = ConstructTileView<STileView>();
+	auto CustomTileViewRef = StaticCastSharedRef<SCustomListView<UObject*>>(TileViewRef);
+
+	CustomTileViewRef->UpdateScrollBarThickness(BarThickness);
+	return CustomTileViewRef;
+}
+
+
+#pragma endregion
+
+
+
 void UMainAgoraUserWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
--- Agora-Unreal-SDK-CPP-Example/Source/AgoraExample/Examples/MainAgoraUserWidget.h
+++ Agora-Unreal-SDK-CPP-Example/Source/AgoraExample/Examples/MainAgoraUserWidget.h
@@ -12,6 +12,57 @@
 #include "LevelSwitchItem.h"
 #include "MainAgoraUserWidget.generated.h"
 
+
+#pragma region CustomTileView
+// To change the scroll bar thickness of the tile view.
+
+UCLASS()
+class AGORAEXAMPLE_API UCustomTileView : public UTileView
+{
+	GENERATED_BODY()
+
+	UCustomTileView(const FObjectInitializer& Initializer);
+
+protected:
+	virtual TSharedRef<STableViewBase> RebuildListWidget() override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Style", meta = (DisplayName = "Scroll Bar Thickness"))
+	FVector2D BarThickness;
+};
+
+
+template <typename ItemType>
+class AGORAEXAMPLE_API SCustomListView : public STileView<ItemType>
+{
+public:
+
+	// It doesn't work
+	//void SetScrollBarThickness(const FVector2D& Vec2)
+	//{
+	//	if (ScrollBar.IsValid())
+	//	{
+	//		ScrollBar->SetThickness(Vec2);
+	//	}
+	//}
+
+	void UpdateScrollBarThickness(const FVector2D& Vec2)
+	{
+		if (STableViewBase::ScrollBar.IsValid())
+		{
+			TSharedPtr<SWidget> SBarParentWidgetPtr = STableViewBase::ScrollBar->GetParentWidget();
+			auto ScrollBarBox = StaticCastSharedPtr<SBox>(SBarParentWidgetPtr);
+
+			ScrollBarBox->SetWidthOverride(FOptionalSize(Vec2.X));
+			STableViewBase::ScrollBar->SetThickness(Vec2);
+		}
+	}
+};
+
+#pragma endregion
+
+
+
 /**
  * 
  */
@@ -24,7 +75,7 @@ public:
 	TArray<FString> LevelArray;
 
 	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
-	UTileView* LevelTileView = nullptr;
+	UCustomTileView* LevelTileView = nullptr;
 
 	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
 	UEditableTextBox* AppidBox;
