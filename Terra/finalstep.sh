dc8801c7b4ab511c72acbc3115c3a68f1528e34e
WinterPu <striveant@outllook.com>
2025-05-16 11:08:49 +0800

    Update finalstep.sh

--- Terra/finalstep.sh
+++ Terra/finalstep.sh
@@ -1,2 +1,8 @@
-cp packages/cpp/AgoraUERTCEngine.h ../Agora-Unreal-SDK-CPP/AgoraPlugin/Source/AgoraPlugin/Public/AgoraCppPlugin/AgoraUERTCEngine.h
-cp packages/cpp/AgoraUERTCEngine.cpp ../Agora-Unreal-SDK-CPP/AgoraPlugin/Source/AgoraPlugin/Private/AgoraCppPlugin/AgoraUERTCEngine.cpp
+#!/usr/bin/env bash
+set -e
+set -x
+MY_PATH=$(realpath $(dirname "$0"))
+PROJECT_ROOT=$(realpath ${MY_PATH})
+
+cp ${PROJECT_ROOT}/packages/cpp/AgoraUERTCEngine.h ${PROJECT_ROOT}/../Agora-Unreal-SDK-CPP/AgoraPlugin/Source/AgoraPlugin/Public/AgoraCppPlugin/AgoraUERTCEngine.h
+cp ${PROJECT_ROOT}/packages/cpp/AgoraUERTCEngine.cpp ${PROJECT_ROOT}/../Agora-Unreal-SDK-CPP/AgoraPlugin/Source/AgoraPlugin/Private/AgoraCppPlugin/AgoraUERTCEngine.cpp
