0a0635222799f1654e00f9009af922401e5b3af6
WinterPu <striveant@outlook.com>
2025-12-26 16:42:28 +0800

    feat: update to 4.5.2.6

--- Agora-Unreal-SDK-CPP/AgoraPlugin/Source/AgoraPlugin/Public/AgoraCppPlugin/AgoraUERTCEngine.h
+++ Agora-Unreal-SDK-CPP/AgoraPlugin/Source/AgoraPlugin/Public/AgoraCppPlugin/AgoraUERTCEngine.h
@@ -31,7 +31,7 @@ namespace agora {
 				static agora::rtc::IRtcEngine* NativeRtcEnginePtr();
 				static FString GetSDKVersion();
 				static void EnableIOSAudioSession(bool bEnable);
-				static void Release(bool sync = false);
+				static void Release();
 				static AgoraAppType RtcEngineAppType;
 
 
@@ -51,7 +51,7 @@ namespace agora {
 
 			private:
 				void CreateEngine();
-				void DestroyEngine(bool sync = false);
+				void DestroyEngine();
 
 
 			public:
