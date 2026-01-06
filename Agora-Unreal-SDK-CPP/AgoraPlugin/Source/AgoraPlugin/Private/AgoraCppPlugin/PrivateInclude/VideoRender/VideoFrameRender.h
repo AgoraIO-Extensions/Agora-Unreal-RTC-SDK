aae439a9157184b234ecf0fa1eaac0ad3a132793
WinterPu <striveant@outlook.com>
2026-01-06 16:40:52 +0800

    feat: add colorspace adaptation

--- Agora-Unreal-SDK-CPP/AgoraPlugin/Source/AgoraPlugin/Private/AgoraCppPlugin/PrivateInclude/VideoRender/VideoFrameRender.h
+++ Agora-Unreal-SDK-CPP/AgoraPlugin/Source/AgoraPlugin/Private/AgoraCppPlugin/PrivateInclude/VideoRender/VideoFrameRender.h
@@ -3,6 +3,7 @@
 #if AGORA_UESDK_ENABLE_VIDEO
 
 #include "DataTypes.h"
+#include "AgoraMediaBase.h"
 
 class UTexture2D;
 class UImage;
@@ -22,20 +23,25 @@ namespace agora {
 
 				void Init(const VideoFrameId & FrameId);
 				void Clear();
-				void OnTick(bool& outRenderRet);
+				void OnTick();
 				
 				void SetRenderImage(UImage* Image);
 
 			private:
 				void InitTexture2D(int Width,int Height);
 				void CopyToTexture2D(VideoFrame* _VideoFrame);
+				
+				// Helper functions for color space handling
+				bool NeedsColorSpaceConversion(const agora::media::base::ColorSpace& colorSpace);
+				void ApplyColorRangeConversion(uint8* PixelData, int Width, int Height, const agora::media::base::ColorSpace& colorSpace);
 
 				void UpdateImage();
 
 				// CPU
 				UTexture2D* RenderTexture = nullptr;
-
-
+				
+				// Store the current color space information
+				agora::media::base::ColorSpace CurrentColorSpace;
 				
 				// Base On Texture
 				UImage* RenderImage = nullptr;
